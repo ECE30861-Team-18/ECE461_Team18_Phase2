f813c0c901dd54fd5e492391a5b808aabed84841
dragyen <johnegewax@gmail.com>
2025-12-13 19:26:56 -0600

    add put to cors headers

--- backend/app/cors.py
+++ backend/app/cors.py
@@ -1,5 +1,5 @@
 CORS_HEADERS = {
     "Access-Control-Allow-Origin": "https://frontend.d23vriwb2bkfqb.amplifyapp.com",
     "Access-Control-Allow-Headers": "Content-Type,Authorization,X-Authorization",
-    "Access-Control-Allow-Methods": "GET,POST,OPTIONS"
+    "Access-Control-Allow-Methods": "GET,POST,OPTIONS,PUT"
 }
